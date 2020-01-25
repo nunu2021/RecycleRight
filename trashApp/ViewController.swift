6771c1a2a8cb825f4cba651d287a20fa85a9f77e
Labdhi Jain <labdhijain753@gmail.com>
2020-01-25 14:27:10 -0600

    Initial Commit

---
+++ trashApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  trashApp
+//
+//  Created by Labdhi Jain on 1/25/20.
+//  Copyright © 2020 Labdhi Jain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
